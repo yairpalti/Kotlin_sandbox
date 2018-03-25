3f0dbfba1d97ecd3d79a63ef7c62223b96dd88f1
Yair Palti <yair@halo-digital.com>
2018-03-25 10:38:11 +0300

    Adding functions example

--- src/Sets.kt
+++ src/Sets.kt
@@ -1,4 +1,6 @@
 fun main (args:Array<String>) {
     var set = setOf(1,4,5,7,5,9,1)
+    var set2 = mutableSetOf(4,4,4,4,2,1)
+    set2.add (5)
     println("set: $set")
 }
---
+++ src/functions.kt
@@ -0,0 +1,16 @@
+
+fun sum (elem1:Double, elem2:Double):Double {
+    return elem1+elem2
+}
+
+fun interSum (elem1:Double, elem2:Double):Unit {
+    println (elem1+elem2)
+}
+fun sum2 (elem1:Double, elem2:Double) = elem1+elem2
+
+fun main(args:Array<String>) {
+    var sum = sum(3.0,5.0)
+    println ("sum: $sum")
+    interSum (7.0,2.0)
+    println (sum2 (6.3,4.4))
+}
