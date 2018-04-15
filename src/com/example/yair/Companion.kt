d59fd65db772fa346a358a3b29939ecd5213af8f
Yair <yairpalti@gmail.com>
2018-04-15 19:03:44 +0300

    changes for companion

--- src/com/example/yair/Companion.kt
+++ src/com/example/yair/Companion.kt
@@ -18,12 +18,30 @@ Able to stand by itself
 Can be used as an expression
 Used for anonymous inner class equivalent
 Lazy
+
+
+A companion object is not part of an instance of a class. You can't access members from a companion object, just like in Java you can't access members from a static method.
+
+Instead, don't use a companion object:
  */
 fun main(args: Array<String>){
+    PersonA.myName = "yair"
+    PersonA.callMe()
     Test.hashCode()
     Thread.sleep(1000)
     Test.ObjectTest.objectMethod()
 
+}
+
+class PersonA (var name:String="yair") {
+    var aaa:Int = 5
+    fun getName1():String {return name}
+    fun getDefaultName():String { return "ccc"}
+    // name of the companion object is omitted
+    companion object {
+        var myName:String = "aaa"
+        fun callMe() = println("I'm called. - $myName")
+    }
 }
 
 class Test {
